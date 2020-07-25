08a746afcd51df4891caeb04aa98dd22c63d1f4a
Tectalaty <66831134+Tectalaty@users.noreply.github.com>
2020-07-25 12:38:43 +0100

    Update SlimeTycoon.lua

--- SlimeTycoon.lua
+++ SlimeTycoon.lua
@@ -4,4 +4,14 @@ FinityWindow.ChangeToggleKey(Enum.KeyCode.Semicolon)
 local FunctionsCategory = FinityWindow:Category("Functions")
 local CreditsCategory = FinityWindow:Category("Credits")
 
-local AutofarmsCat = FunctionsCategory:Sector("Autofarms")
+local MoneyCat = FunctionsCategory:Sector("Money")
+local CreditCat = FunctionsCategory:Sector("Credits")
+
+MoneyCat:Cheat("Button", "Inf Money", function()
+local A_1 = math.huge
+local A_2 = "Code1"
+local Event = game:GetService("ReplicatedStorage").EnterCode
+Event:FireServer(A_1, A_2)
+end)
+
+CreditCat:Cheat("Label", "Trash Vk")
