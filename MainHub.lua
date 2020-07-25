fd967d3eeb19459b5c3715de875413fbae81ddfc
Tectalaty <66831134+Tectalaty@users.noreply.github.com>
2020-07-25 14:15:30 +0100

    Update MainHub.lua

--- MainHub.lua
+++ MainHub.lua
@@ -3,5 +3,5 @@ local x = game.PlaceId
 local SlimeTycoon = 3596472165
 
 if x == SlimeTycoon then
-  local Finity = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tectalaty/Derp-Hu-V2/master/FinityUILib"))()
+  local Finity = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tectalaty/Derp-Hu-V2/master/SlimeTycoon.lua"))()
 end
