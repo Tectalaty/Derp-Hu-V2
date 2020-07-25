ff0138beeb0ce35559a481bc7d9c7825c709723d
Tectalaty <66831134+Tectalaty@users.noreply.github.com>
2020-07-25 12:35:31 +0100

    Add files via upload

---
+++ SlimeTycoon.lua
@@ -0,0 +1,7 @@
+local FinityWindow = Finity.new(true)
+FinityWindow.ChangeToggleKey(Enum.KeyCode.Semicolon)
+
+local FunctionsCategory = FinityWindow:Category("Functions")
+local CreditsCategory = FinityWindow:Category("Credits")
+
+local AutofarmsCat = FunctionsCategory:Sector("Autofarms")
