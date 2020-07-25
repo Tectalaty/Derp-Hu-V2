8b80f2a0a03a42d4ebbbb70178907884a21b3112
Tectalaty <66831134+Tectalaty@users.noreply.github.com>
2020-07-25 16:17:16 +0100

    Update SupePowerSimulator.lua

--- SupePowerSimulator.lua
+++ SupePowerSimulator.lua
@@ -9,9 +9,15 @@ local CreditsCategory = FinityWindow:Category("Credits")
 local AutofarmsCat = FunctionsCategory:Sector("Autofarms")
 local TeleportsCat = FunctionsCategory:Sector("Teleports")
 
-AutofarmsCat:Cheat("Checkbox","ESP Enabled", function(State)
+AutofarmsCat:Cheat("Checkbox","Spam Quest", function(State)
 		if state then
-_G.somin = true
+_G.spamquests = true
+
+while _G.spamquests == true do wait(1)
+local A_1 = "GamesReborn"
+local Event = game:GetService("ReplicatedStorage").Events.Quest
+Event:FireServer(A_1)
+end
      else
-      _G.somin = false
-	end)
+      _G.spamquests = true
+end)
